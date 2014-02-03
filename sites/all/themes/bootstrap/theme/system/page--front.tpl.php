de96a5f666926b3f77cd43f979be6696d03898d7
Nayeon Kim <nayeonki@usc.edu>
2014-02-03 01:39:04 -0800

    Add CSS and images for homepage look and feel

--- sites/all/themes/bootstrap/theme/system/page--front.tpl.php
+++ sites/all/themes/bootstrap/theme/system/page--front.tpl.php
@@ -20,7 +20,7 @@
       </button>
     </div>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+    
       <div class="navbar-collapse collapse">
         <nav role="navigation">
           
@@ -31,26 +31,31 @@
           <?php endif; ?>
         </nav>
       </div>
-    <?php endif; ?>
+    
   </div>
 </header>
 
 
 <div class="main-container container">
   <div id="video-section">
-    <a id="play-btn" href="#"></a>
-    <a id="view-btn" href="#" target="_blank">View open positions &raquo;</a>
+    <img src="<?php print $directory; ?>/img/main.png"/>
+    <div id="pulldown">
+      <h2>Build relevant products.. Join Bozway!</h2>
+      <a href="#dept-section"><strong>View open positions</strong><br/>
+      <strong>&#9660;</strong></a>
+    </div>
   </div>
   <div class="content-section">
     <div class="row">
       <div class="col-md-6 content">
-        <img src="<?php print $directory; ?>/images/img.jpg" style="width:100%">
+        <img src="<?php print $directory; ?>/img/img01.jpg" style="width:100%">
       </div>
       <div class="col-md-6 content">
         <h2>Why work at Will Rain IT?</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu scelerisque neque. Sed sagittis lectus eget sollicitudin venenatis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
       </div>
     </div>
+    <hr style="border-top: 5px #794d1c solid">
   </div>
   <div class="content-section">
     <div class="row">
@@ -59,38 +64,45 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu scelerisque neque. Sed sagittis lectus eget sollicitudin venenatis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
       </div>
       <div class="col-md-6" style="text-align:right;">
-        <img src="<?php print $directory; ?>/images/img.jpg" style="width:100%" >
+        <img src="<?php print $directory; ?>/img/img02.jpg" style="width:100%" >
       </div>
     </div>
+    <hr style="border-top: 5px #794d1c solid">
   </div>
   <div id="dept-section">
     <div id="dept-section-inner">
+      <h3>Now hiring in: </h3>
       <div class="row">
         <div class="col-md-4">
-          <a href="#"><img src="<?php print $directory; ?>/images/img.jpg"></a>
-          Software Engineering
+          <a href="software-engineering"><img src="<?php print $directory; ?>/img/dept01.png">
+          <strong>Software Engineering</strong></a>
         </div>
         <div class="col-md-4">
-          <a href="#"><img src="<?php print $directory; ?>/images/img.jpg"></a>
-          Business Development
+          <a href="art-direction"><img src="<?php print $directory; ?>/img/dept03.png">
+          <strong>Design/Art Direction</strong></a>
         </div>
         <div class="col-md-4">
-          <a href="#"><img src="<?php print $directory; ?>/images/img.jpg"></a>
-          PR&Marketing
+          <a href="business-development"><img src="<?php print $directory; ?>/img/dept02.png">
+          <strong>Business Development</strong></a>
         </div>
       </div>
       <div class="row">
         <div class="col-md-4">
-          <a href="#"><img src="<?php print $directory; ?>/images/img.jpg"></a>
-          Software Engineering
+          <a href="it-security"><img src="<?php print $directory; ?>/img/dept04.png">
+          <strong>IT&Security</strong></a>
         </div>
         <div class="col-md-4">
-          <a href="#"><img src="<?php print $directory; ?>/images/img.jpg"></a>
-          Business Development
+          <a href="project-management"><img src="<?php print $directory; ?>/img/dept05.png">
+          <strong>Project Management</strong></a>
         </div>
         <div class="col-md-4">
-          <a href="#"><img src="<?php print $directory; ?>/images/img.jpg"></a>
-          PR&Marketing
+          <a href="pr-marketing"><img src="<?php print $directory; ?>/img/dept06.png">
+          <strong>PR/Marketing</strong></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <a href="all-positions">View all open positions &raquo;</a>
         </div>
       </div>
 
